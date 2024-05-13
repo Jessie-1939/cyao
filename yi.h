6d6c6b8648058eb0d1d259b49b1253f1a43043a8
Jessie-1939 <2087684828@qq.com>
2024-05-13 15:25:40 +0800

    易原始

---
+++ yi.h
@@ -0,0 +1,21 @@
+
+#ifndef YI_H
+#define YI_H
+
+// 八卦名称
+const char* hexagrams[] = {
+    "乾",
+    "坤",
+    "屯",
+    "蒙",
+    "需",
+    "讼",
+    "师",
+    "比",
+   
+};
+
+// 获取八卦名称的函数声明
+const char* get_hexagram_name(int num);
+
+#endif // YI_H
